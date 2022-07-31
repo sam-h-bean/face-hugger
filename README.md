3504d0ceadfce5ba5f795123ff0a4e70674ce6aa
Sam Bean <sam.h.bean@gmail.com>
2022-07-30 22:22:45 -0400

    deploy tensorrt

--- README.md
+++ README.md
@@ -10,8 +10,12 @@ In order to not use the same pod resources for both serving and exporting I have
 used to load the model for inference in the serving pod.
 
 ## TensorRT Conversion (In Progress)
-Sine TensorRT gives better performance than even level 99 ONNX optimized graph on GPU we will try to
+Since TensorRT gives better performance than even level 99 ONNX optimized graph on GPU we will try to
 convert the ONNX graph to TensorRT and host that.
+
+[transformer-deploy](https://github.com/ELS-RD/transformer-deploy) has done a lot of interesting work
+writing the glue code to make TensorRT and Triton easier to use. We will try to use their utility
+methods  to convert our ONNX model to TensorRT.
 
 ## Triton
 This graph is then hosted for inference on a [Triton server](https://github.com/triton-inference-server/server) deployed in Kubernetes. The server is exposed through a Kubernetes LoadBalancer where outside
--- export_model_to_repository.py
+++ export_model_to_repository.py
@@ -1,13 +1,29 @@
 import os
 import shutil
+import tensorrt as trt
 
 from optimum.onnxruntime import ORTModelForCausalLM
+from .trt_utils import build_engine, save_engine
 
 if __name__ == "__main__":
     repository_path = "/var/triton/gpt2/repository/"
     model_id = "distilgpt2"
     model = ORTModelForCausalLM.from_pretrained(
         model_id, from_transformers=True
+    )
+    model.save_pretrained("./", file_name="model.onnx")
+    trt_logger = trt.Logger(trt.Logger.ERROR)
+    runtime = trt.Runtime(trt_logger)
+    engine = build_engine(
+        runtime=runtime,
+        onnx_file_path="model.onnx",
+        logger=trt_logger,
+        min_shape=(1, 1),
+        optimal_shape=(1, 128),  # num beam, batch size
+        max_shape=(1, 384),  # num beam, batch size
+        workspace_size=10000 * 1024 ** 2,
+        fp16=True,
+        int8=False,
     )
 
     # Save tokenizer for use by triton
@@ -19,9 +35,10 @@ if __name__ == "__main__":
 
     # Save model for use by triton
     model_path = repository_path + "model/"
-    os.makedirs(model_path, exist_ok=True)
+    os.makedirs(model_path + "1/", exist_ok=True)
     shutil.copy("triton-config/model/config.pbtxt", model_path + "config.pbtxt")
-    model.save_pretrained(model_path + "1/", file_name="model.onnx")
+    save_engine(engine, "model.plan")
+    shutil.copy("model.plan", model_path + "1/config.pbtxt")
 
     # Save pipeline for use by triton
     transformer_path = repository_path + "transformer/"
